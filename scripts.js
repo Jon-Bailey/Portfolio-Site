59815f68ad0520badc02721c66556dfbf5368cea
Jon Bailey <jonbailey788@gmail.com>
2021-02-27 11:53:20 -0600

    Second Project Page and Card sizing

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+//Eventually make it so there is one project.html file with images and text loaded in dynamically...
+//Right now having something up is better than nothing?
+
 // hero section fad in
 gsap.from('#hero-text', { duration: 1, x:-100 });
 gsap.from('.icons', { duration:1, x:100 });
+//good job gsap, you did it.
