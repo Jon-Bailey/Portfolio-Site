66e56da5f5e6180367eb191918f3299a058831be
Jon Bailey <jonbailey788@gmail.com>
2021-03-12 21:55:02 -0600

    project 3 modal

---
+++ js/project3.js
@@ -0,0 +1,16 @@
+// Modal
+var modal = document.getElementById("myModal");
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
