b4e8f1c67e9dc874c2aff00255ce070613615678
Jon Bailey <jonbailey788@gmail.com>
2021-07-02 17:26:13 -0500

    sizing and alt hero background

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 // hero section fade in
-gsap.from('#hero-text', { duration: 1, x:-100 });
-gsap.from('.icons', { duration:1, x:100 });
+gsap.from('#hero-text', { duration: 1, y:-100 });
+//gsap.from('.icons', { duration:1, x:100 });
 //good job gsap, you did it.
